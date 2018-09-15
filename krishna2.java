7099da29d670875383206990cd8bccc8f45151d2
krishna2512 <43032426+krishna2512@users.noreply.github.com>
2018-09-15 15:36:23 +0530

    Add files via upload

---
+++ krishna2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.*;
+class Main{
+	public static void main (String[] args)
+  {
+		Scanner sc=new Scanner(System.in);
+    String name,city;
+    int age,height;
+    System.out.println("enter the name");
+   name=sc.next();
+    System.out.println("enter the city");
+   city=sc.next();
+    System.out.println("enter the age");
+    age=sc.nextInt();
+    System.out.println("enter the height");
+   height=sc.nextInt();
+   name=name;
+    age=age;
+    city=city;
+    height=height;
+     System.out.println("name=:"+name);
+      System.out.println("age=:"+age);
+       System.out.println("city=:"+city);
+        System.out.println("height=:"+height);
+	}
+}
