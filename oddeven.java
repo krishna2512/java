dd28692eb0bbfbff3f152ebaf10e0ceb41f2f728
krishna2512 <43032426+krishna2512@users.noreply.github.com>
2018-10-04 11:32:03 +0530

    Update oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
+        Scanner number = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
-        int num = reader.nextInt();
+        int num = number.nextInt();
 
         if(num % 2 == 0)
             System.out.println(num + " is even");
