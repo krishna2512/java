2efdd08bf88173673e6a2f18d1f2a6e4bf230c28
krishna2512 <43032426+krishna2512@users.noreply.github.com>
2018-10-05 12:04:28 +0530

    Update largestnum.java

--- largestnum.java
+++ largestnum.java
@@ -3,11 +3,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        double n1, n2, n3;
+       int n1, n2, n3;
         Scanner sc=new Scanner(System.in);
-        n1=sc.nextDouble();
-        n2=sc.nextDouble();
-        n3=sc.nextDouble();
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        n3=sc.nextInt();
 
         if( n1 >= n2 && n1 >= n3)
             System.out.println(n1 + " ");
