118b74a7460e5326a45fcff3847dbcf639a7058a
krishna2512 <43032426+krishna2512@users.noreply.github.com>
2018-10-05 12:20:54 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        int year;
+        Scanner sc=new Scanner(System.in);
+        year=sc.nextInt();
+         boolean leap = false;
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+    
+             if ( year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+            else
+                leap = true;
+        }
+        else
+            leap = false;
+
+        if(leap)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
